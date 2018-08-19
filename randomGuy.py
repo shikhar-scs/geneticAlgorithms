3d7523792eb3128277781f4c9abcc842c72b69d0
shikhar-scs <shikhar9a@gmail.com>
2018-08-19 18:24:37 +0530

    complete

--- randomGuy.py
+++ randomGuy.py
@@ -9,6 +9,11 @@ health_cost = [0.10, 0.11, 0.29, 0.32, 0.19, 0.14, 0.17, 0.29, 0.53, 0.10,
 
 complete_pop = [['chromosome', 'no_of_set_bits', 'learning_index', 'health_cost', 'li-hc', 'set_bits']]
 index = 0
+best_in_i_generation = []
+
+with open('next_gen.csv', 'w') as writeFile:
+    writer = csv.writer(writeFile)
+    writer.writerows([])
 
 
 def cross_and_mute(p1, p2):
@@ -38,6 +43,58 @@ def cross_and_mute(p1, p2):
     return [new_child, new_child.count('1'), str(l_i)[0:4], str(h_c)[0:4], str(l_i - h_c)[0:4], set_bits]
 
 
+def master_function(first_gen):
+    new_next_gen = []
+
+    first_gen = sorted(first_gen, key=operator.itemgetter(1), reverse=True)
+
+    for i in range(0, len(first_gen)):
+        if first_gen[i][1] < 11:
+            first_gen = first_gen[0:i]
+            break
+
+    # generating next generation pairs
+    next_gen_probs = []
+    for i in range(0, round(len(first_gen))):
+        p1 = 0
+        p2 = 0
+
+        while p1 == p2:
+            p1 = round((100 * random.random())) % len(first_gen)
+            p2 = round((100 * random.random())) % len(first_gen)
+        next_gen_probs.append([p1, p2])
+
+    next_gen = []
+
+    for i in range(0, len(next_gen_probs)):
+        new_guy = cross_and_mute(first_gen[next_gen_probs[i][0]][0], first_gen[next_gen_probs[i][1]][0])
+        new_guy.append(str(next_gen_probs[i][0]) + ' ' + str(next_gen_probs[i][1]))
+        next_gen.append(new_guy)
+        complete_pop.append(new_guy)
+
+    next_gen = sorted(next_gen, key=operator.itemgetter(4), reverse=True)
+    best_in_i_generation.append(next_gen[0])
+    good_parents = []
+
+    for i in range(0, round(len(next_gen) / 2)):
+        p1 = int(next_gen[i][6].split(' ')[0])
+        p2 = int(next_gen[i][6].split(' ')[1])
+        new_next_gen.append(next_gen[i])
+        if p1 not in good_parents:
+            good_parents.append(p1)
+        if p2 not in good_parents:
+            good_parents.append(p2)
+
+    for i in range(0, len(good_parents)):
+        new_next_gen.append(first_gen[good_parents[i]])
+
+    with open('next_gen.csv', 'a') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerows(next_gen)
+
+    return new_next_gen
+
+
 # the first population
 for i in range(0, 20):
     a = str(bin(int(round(100000054432077770*random.random())))).split('0b')[1]
@@ -55,44 +112,18 @@ for i in range(0, 20):
         complete_pop.append([a, a.count('1'), str(l_i)[0:4], str(h_c)[0:4], str(l_i - h_c)[0:4], set_bits, 'F'])
 
 # those having no. of set bits > 10 (minimum 50% attendance)
-first_gen = sorted(complete_pop[1:len(complete_pop)], key=operator.itemgetter(1), reverse=True)
-for i in range(0, len(first_gen)):
-    if first_gen[i][1] < 11:
-        first_gen = first_gen[0:i]
-        break
+last_child_gen = master_function(master_function(master_function(master_function(master_function(sorted(complete_pop[1:len(complete_pop)], key=operator.itemgetter(1), reverse=True))))))
 
-
-with open('first_gen.csv', 'w') as writeFile:
-    writer = csv.writer(writeFile)
-    writer.writerows(first_gen)
-
-# generating next generation pairs
-next_gen_probs = []
-for i in range(0, round(len(first_gen))):
-    p1 = 0
-    p2 = 0
-
-    while p1 == p2:
-        p1 = round((100 * random.random())) % len(first_gen)
-        p2 = round((100 * random.random())) % len(first_gen)
-    next_gen_probs.append([p1, p2])
-
-next_gen = []
-
-for i in range(0, len(next_gen_probs)):
-    new_guy = cross_and_mute(first_gen[next_gen_probs[i][0]][0], first_gen[next_gen_probs[i][1]][0])
-    new_guy.append(str(next_gen_probs[i][0]) + ' ' + str(next_gen_probs[i][1]))
-    next_gen.append(new_guy)
-    complete_pop.append(new_guy)
-
-next_gen = sorted(next_gen, key=operator.itemgetter(4), reverse=True)
-
-
-with open('next_gen.csv', 'w') as writeFile:
-    writer = csv.writer(writeFile)
-    writer.writerows(next_gen)
+best_in_last_generation = best_in_i_generation[len(best_in_i_generation)-1]
+print(best_in_last_generation)
+best_overall = sorted(last_child_gen, key=operator.itemgetter(4), reverse=True)[0]
+print(best_overall)
 
 with open('complete_population.csv', 'w') as writeFile:
     writer = csv.writer(writeFile)
     writer.writerows(complete_pop)
+
+with open('best_in_i_gen.csv', 'w') as writeFile:
+    writer = csv.writer(writeFile)
+    writer.writerows(best_in_i_generation)
 
